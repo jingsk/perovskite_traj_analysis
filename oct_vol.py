f9d5742a0695179ab08e813b84f3992b3dcfea1a
jingsk <jingsk129@gmail.com>
2024-06-06 09:42:08 -0700

    modulize oct_vol.py module - now oct_vol is callable

--- oct_vol.py
+++ oct_vol.py
@@ -27,63 +27,48 @@ def trim(src,
         argsort = np.delete(argsort, 0)
     mask = (mask) & dis_mask
     return mask
+
+def oct_vol(Ni_idx, atoms, r_max):
+    edge_src, edge_dst, edge_vector = neighbor_list("ijD",
+                                                    a=atoms,
+                                                    cutoff=r_max)
     
+    #flag for incomplete octahedron [typical defects - tetradedral defect, vacancies]
+    oct_vol = []
+    oct_dst = []
+    oct_mask = []
+    oct_points = []
+    # H_dist = []
+    for src in tqdm(Ni_idx):
+        #print(src)
+        dst = edge_dst[edge_src==src]
+        vec = edge_vector[edge_src==src]
+        #print(dst)
+        #print(vec)
+        mask = trim(src, dst, vec)
+        points = vec[mask]
+        oct_dst.append(dst[mask])
+        oct_points.append(points)
+        oct_mask.append(mask)
+    
+        try:
+            oct_vol.append(octahedral_volume1(*points))
+        except TypeError:
+            #raise Exception("missing one octahedron here")
+            #print("missing one octahedron here")
+            #print(points)
+            oct_vol.append(-1)
+    
+    return np.array(oct_vol)
 
 
-#atoms = read('./01_SCF_111k_400eV/POSCAR')
-#atoms = read('./pristine/01_SCF_111k_400eV/POSCAR')
-#atoms = read('/lcrc/globalscratch/jingt/perovskite/Nd/relax/04_OPT_sukriti/CONTCAR')
-atoms = read(sys.argv[1],sys.argv[2])
-#atoms = read('supercell_18_2.poscar')
-#atoms = read('supercell_18.poscar')
-
-Ni_idx = np.array([atom.index for atom in atoms if atom.symbol=='Ni'])
-#print(Ni_idx) 
-r_max = 7 #/ (6) * (atoms.get_volume()) ** (1/3)
-edge_src, edge_dst, edge_vector = neighbor_list("ijD",
-                                                a=atoms,
-                                                cutoff=r_max)
-edge_src2, edge_dst2, edge_vector2 = neighbor_list("ijD",
-                                                a=atoms,
-                                                cutoff=7)
-#flag for incomplete octahedron [typical defects - tetradedral defect, vacancies]
-oct_vol = []
-oct_dst = []
-oct_mask = []
-oct_points = []
-H_dist = []
-for src in tqdm(Ni_idx):
-    #print(src)
-    dst = edge_dst[edge_src==src]
-    vec = edge_vector[edge_src==src]
-    #print(dst)
-    #print(vec)
-    mask = trim(src, dst, vec)
-    points = vec[mask]
-    oct_dst.append(dst[mask])
-    oct_points.append(points)
-    oct_mask.append(mask)
-    mask = trim(src = src, 
-                dst = edge_dst2[edge_src2==src],
-                vec = edge_vector2[edge_src2==src], 
-                species='H',
-                minimum=1
-               )
-    try:
-        oct_vol.append(octahedral_volume1(*points))
-    except TypeError:
-        #raise Exception("missing one octahedron here")
-        #print("missing one octahedron here")
-        #print(points)
-        oct_vol.append(-1)
-    try: 
-        H_dist.append(np.linalg.norm(edge_vector2[edge_src2==src][mask]))
-    except IndexError:
-        H_dist.append(-1)
-
-oct_vol =np.array(oct_vol)
-
-np.savetxt('vol.csv', 
-           np.vstack([Ni_idx, oct_vol]).T, 
-           fmt=['%d', '%.4f'])
-
+if __name__ == "__main__":
+    atoms = read(sys.argv[1],sys.argv[2])
+    Ni_idx = np.array([atom.index for atom in atoms if atom.symbol=='Ni'])
+    oct_vol = oct_vol(Ni_idx = Ni_idx,
+                      atoms = atoms,
+                      r_max = 7)
+    np.savetxt('vol.csv', 
+               np.vstack([Ni_idx, oct_vol]).T, 
+               fmt=['%d', '%.4f'])
+    
